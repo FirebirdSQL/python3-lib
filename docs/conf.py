9f46d250ad3e1a0de90c8bbf7d2a5e2a14edc496
Pavel Cisar <pcisar@ibphoenix.cz>
2022-10-03 18:09:29 +0200

    Release v1.2.1; Documention

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,14 @@ import sphinx_bootstrap_theme
 # -- Project information -----------------------------------------------------
 
 project = 'firebird-lib'
-copyright = '2020-2021, The Firebird Project'
+copyright = '2020-2022, The Firebird Project'
 author = 'Pavel Císař'
 
 # The short X.Y version
-version = '1.2.0'
+version = '1.2.1'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.1'
 
 
 # -- General configuration ---------------------------------------------------
